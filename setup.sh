5c1504723baefd0d781dc9ab457736a5bbcc8fb8
Ronald Otieno <ronnlats@gmail.com>
2023-07-22 04:47:53 +0000

    basic setups

--- setup.sh
+++ setup.sh
@@ -1,11 +1,18 @@
 #! /bin/bash
 echo "Setup has began..."
-zsh
-wget https://github.com/RonaldLat/ronfiles/blob/gh-cs/nvim.appimage
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-ln -s ~/workspace/.zshrc ~/.zshrc
-sudo apt-get install fuse libfuse2
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended || true
+#zshrc
+rm -r ~/.zshrc || true
+cd ~/ && ln -fs /workspace/workspace/.zshrc .zshrc && cd /workspace/workspace || true
+#tmux
+rm -r ~/tmux.conf || true
+cd ~/ && ln -fs /workspace/workspace/tmux.conf tmux.conf && cd /workspace/workspace || true
+#nvim
+rm -r ~/.config/nvim || true
+cd ~/.config && ln -fs /workspace/workspace/nvim nvim && cd /workspace/workspace || true
+sudo apt-get install fuse libfuse2 tmux -yf
 source ~/.zshrc
-cd /workspaces
+#cd /workspaces
+zsh
 
 echo "Setup END!"
